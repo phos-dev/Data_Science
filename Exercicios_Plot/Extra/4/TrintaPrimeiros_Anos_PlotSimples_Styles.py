b5e117a878de466910eb9511049b9c22230dda3a
Pedro2103 <pedro.o.silva.henrique@gmail.com>
2019-12-15 19:50:21 -0300

    Padrões Atribuídos

--- Exercicios_Plot/Extra/4/TrintaPrimeiros_Anos_PlotSimples_Styles.py
+++ Exercicios_Plot/Extra/4/TrintaPrimeiros_Anos_PlotSimples_Styles.py
@@ -4,8 +4,8 @@ import pandas as pd
 file = pd.read_csv("/home/phos/Desktop/time-series-pam/a2_MANCHAS.csv")
 
 file[:30].plot("Ano","manchas", label="Manchas", grid=True)
-plt.title("Aparecimento de manchas solares p/ ano", fontsize=17, color="r", style="italic", family="monospace")
+plt.title("Aparecimento de manchas solares p/ ano (30 Primeiros Anos)", fontsize=12, color="r", style="italic", family="monospace")
 plt.xlabel("Ano", fontsize=13); plt.ylabel("Qtd. de manchas", fontsize=13)
 plt.xticks(rotation="vertical")
 plt.tight_layout()
-plt.savefig("Boxplot_M")
+plt.savefig("TrintaPrimeiros_Anos_Styles")
